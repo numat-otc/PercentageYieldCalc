8d4123c526c821989741e4ecea9531452732f89a
numat <numat@stu.otc.school.nz>
2021-11-08 15:16:45 +1300

    first test of concept file

---
+++ ReInvestmentCalculator.py
@@ -0,0 +1,22 @@
+import os
+
+while True:
+    os.system("cls")
+    i=int(input("Initial Investment: $"))
+    r=int(input("Annual Interest Rate (%): "))
+    p=int(input("Over How Many Years: "))
+    a=int(input("Reinvestment Amount (at the end of period): "))
+    f=int(input("Reinvestment Frequency: "))
+
+    os.system("cls")
+    RATE = r/100+1
+    T = i*RATE
+    for x in range(p):
+        T=T*RATE+a
+    T = round(T,2)
+    print("TOTAL: ${}".format(T))
+    print("Initial Investment: ${}".format(i))
+    print("Annual Interest Rate: {}%".format(r))
+    print("After period reinvestment amount: ${}".format(a))
+    print("reinvestment frequency (per period): {}".format(f))
+    input()
