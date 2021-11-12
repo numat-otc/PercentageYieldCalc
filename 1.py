5e1bf4f26d62644124f69db28fcd42c8e0fd66cb
treyn <numat@stu.otc.school.nz>
2021-11-12 21:26:37 +1300

    proof of concept code finished

--- 1.py
+++ 1.py
@@ -1,7 +1,7 @@
 import os
 
-while True:
-    os.system("cls")
+while True: # repeat
+    os.system("cls") # clear screen
     i=int(input("Initial Investment: $")) # starting amount, can be 0
     r=float(input("Annual percentage yield: (e.g: 8.5): ")) # APY, e.g 8.5% interest per year so 108.5% so x1.085
     p=int(input("Over How Many Years: ")) # time period (in years)
@@ -12,12 +12,12 @@ while True:
     RATE = r/100+1 # converts rate from 8.5% to x1.085
     T = i*RATE # first year
     for x in range(1,p): # repeat
-        T=T*RATE+a
-        record.append(round(T,2))
-    T = round(T,2)
+        T=T*RATE+a # Total = (previous total) X (Annual Percentage Yield) + (reinvestment amount
+        record.append(round(T,2)) # add Total to a record, rounded to 2 dp
+    T = round(T,2) # Round total to 2 dp
 
-    os.system("cls")
-    print("TOTAL: ${}".format(T))
+    os.system("cls") # clear screen
+    print("TOTAL: ${}".format(T)) # print all variables with context
     print("Initial Investment: ${}".format(i))
     print("Annual Interest Rate: {}%".format(r))
     print("Years: {}".format(p))
@@ -25,3 +25,5 @@ while True:
     print("reinvestment frequency (per period): {}".format(f))
     print("Record: {}".format(record))
     input()
+
+    # this program is a proof of concept and allows me to develop a better version
